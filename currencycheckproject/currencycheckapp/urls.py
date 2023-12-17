815468eb92bb00e42b65a49b4fad8cdb89deea6e
Jacek <jacekszwarc1@gmail.com>
2023-12-17 09:33:19 +0100

    Move work plan to ProjectStructure.txt file

--- currencycheckproject/currencycheckapp/urls.py
+++ currencycheckproject/currencycheckapp/urls.py
@@ -11,22 +11,3 @@ urlpatterns = [
     ]
 
 
-# TODO MAIN
-# 1 (DONE) Fix EUR parsing 
-# 2 (DONE) Add "go back to LoadCurrencyData" on Display site and the other way "go to LoadCurrencyDataView"
-# 3 (DONE) Save previously added currencies so User dont need to name the 
-# ones for scraping at that moment
-# - Append new currencies in currency_shortcut (handle duplicates, how to delete unwanted currencies?)
-# 4 (DONE) Add deletion of currencies in list_user_currencies/
-# 5 (DONE) Add automatic scraping after entering /display_currencies/ url
-# 6 (DONE) Add validation (did user provided the correct names of the currency?)
-# 7 (DONE) Rename functions, variables, classes to be more descriptive
-# 8 (DONE) User input currencies "toUpper"
-# 9 (DONE) Use Docker
-# 10 
-# - (DONE) Send mail with notification about exceeted currency values
-# - Ability to assign upper and lower limits for a specific currency
-# - Testing these currency boundaries in real time
-# 11 Logging with OAuth
-# 12 Testing
-# 13 Github Docs - Create understandable and professional usage documentation 
