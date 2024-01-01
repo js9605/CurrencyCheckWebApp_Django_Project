5b8315c28b35569bbda8bba398026f27a07a7790
Jacek <jacekszwarc1@gmail.com>
2024-01-01 19:07:49 +0100

    Call Django setup to initialize the application before autodiscovering celery tasks

--- currencycheckproject/currencycheckproject/celery.py
+++ currencycheckproject/currencycheckproject/celery.py
@@ -1,16 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django import setup
 from django.apps import apps
-
 
 print(f"DEBUG: DJANGO_SETTINGS_MODULE={os.environ['DJANGO_SETTINGS_MODULE']}")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
 app = Celery('currencycheckproject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Call Django setup to initialize the application
+setup()
+
 def autodiscover_tasks():
     for app_config in apps.get_app_configs():
+        # Look for celery_tasks.py instead of tasks.py
         app_config_path = app_config.path.replace('.', '/')
         app_tasks_module = f'{app_config_path}/tasks/celery_tasks.py'
         try:
@@ -18,4 +22,5 @@ def autodiscover_tasks():
         except ImportError:
             print("ImportError in celery.py")
 
+# Call the custom autodiscover_tasks function
 autodiscover_tasks()
