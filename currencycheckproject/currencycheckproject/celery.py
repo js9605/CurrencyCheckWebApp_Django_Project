c4561f2a78f79ade87e5adbce1492991c30fd5cf
Jacek <jacekszwarc1@gmail.com>
2024-01-01 18:59:14 +0100

    Update autodiscover_tasks to find custom celery_tasks.py

--- currencycheckproject/currencycheckproject/celery.py
+++ currencycheckproject/currencycheckproject/celery.py
@@ -1,10 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 import os
-import sys
 from celery import Celery
+from django.apps import apps
 
 
 print(f"DEBUG: DJANGO_SETTINGS_MODULE={os.environ['DJANGO_SETTINGS_MODULE']}")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
 app = Celery('currencycheckproject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+def autodiscover_tasks():
+    for app_config in apps.get_app_configs():
+        app_config_path = app_config.path.replace('.', '/')
+        app_tasks_module = f'{app_config_path}/tasks/celery_tasks.py'
+        try:
+            __import__(app_tasks_module)
+        except ImportError:
+            print("ImportError in celery.py")
+
+autodiscover_tasks()
