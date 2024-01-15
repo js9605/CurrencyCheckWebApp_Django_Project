f599e5da440ee96b2432cacf212c32fb605c9867
Jacek <jacekszwarc1@gmail.com>
2024-01-15 21:46:07 +0100

    celery_beat setup

--- currencycheckproject/currencycheckproject/celery.py
+++ currencycheckproject/currencycheckproject/celery.py
@@ -19,25 +19,8 @@ def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
 
-# Schedule periodic task to check currency values #TODO Not working [?] set up Celery Beat to schedule periodic tasks 
-# Ive put it in func last time to check if  @app.task(bind=True) will trigger beat_schedule
-# @app.task(bind=True)
-# def main_celery():
-#     app.conf.beat_schedule = {
-#         'check-currency-every-hour': {
-#             'task': 'currencycheckapp.services.notification_handler.fetch_currency_values_and_notify',
-#             # 'schedule': crontab(minute=0, hour='*'),
-#             'schedule': timedelta(minutes=2),
-#             'args': (),
-#             'kwargs': {
-#                 'user_email_func': 'currencycheckapp.services.notification_handler.get_user_email',
-#                 'currency_value_func': 'currencycheckapp.services.notification_handler.get_currency_value',
-#                 'threshold_func': 'currencycheckapp.services.notification_handler.get_threshold',
-#             },
-#         },
-#     }
-
-#     app.conf.timezone = 'UTC'
+# Schedule periodic task to check currency values 
+#TODO Not working [?] set up Celery Beat to schedule periodic tasks 
     
 app.conf.beat_schedule = {
     'check-currency-every-hour': {
