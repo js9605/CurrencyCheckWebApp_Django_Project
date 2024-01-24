027a1551136558dd8f454fc455794726bddbe49f
Jacek <jacekszwarc1@gmail.com>
2024-01-24 17:21:58 +0100

    Condition check
    I've added separate conditions for checking the lower and upper limits. If the lower_limit is not None and the purchase_rate exceeds it, or if the upper_limit is not None and the selling_rate is below it, the corresponding email notifications will be sent.

--- currencycheckproject/currencycheckapp/tasks/celery_tasks.py
+++ currencycheckproject/currencycheckapp/tasks/celery_tasks.py
@@ -6,12 +6,24 @@ def check_currency_threshold(user_email, currency_shortcut, currency_value, thre
     
     print('DEBUG: Enter check_currency_threshold')
 
-    if currency_value['purchase_rate'] > threshold['lower_limit'] or currency_value['selling_rate'] < threshold['upper_limit']: 
+    lower_limit = threshold['lower_limit']
+    upper_limit = threshold['upper_limit']
+
+    if lower_limit is not None and currency_value['purchase_rate'] > lower_limit:
         subject = 'Currency Threshold Exceeded'
-        message = f'The {currency_shortcut} value ({currency_value}) has exceeded the threshold ({threshold}).'
+        message = f'The {currency_shortcut} purchase rate ({currency_value["purchase_rate"]}) has exceeded the lower limit ({lower_limit}).'
         from_email = 'tempsurnametempname2@gmail.com'
         recipient_list = [user_email]
         send_mail(subject, message, from_email, recipient_list)
 
+    if upper_limit is not None and currency_value['selling_rate'] < upper_limit:
+        subject = 'Currency Threshold Exceeded'
+        message = f'The {currency_shortcut} selling rate ({currency_value["selling_rate"]}) has exceeded the upper limit ({upper_limit}).'
+        from_email = 'tempsurnametempname2@gmail.com'
+        recipient_list = [user_email]
+        send_mail(subject, message, from_email, recipient_list)
+
+    else:
+        print("DEBUG: some values are None")
 
     print("DEBUG: check_currency_threshold task completed.")
