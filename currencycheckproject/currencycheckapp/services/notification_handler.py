c3f680f33553a7888a767f02e79e1e641b14c4fe
Jacek <jacekszwarc1@gmail.com>
2024-01-26 19:17:48 +0100

    Fix different values of threshold in views.py and in notification_handler.get_threshold

--- currencycheckproject/currencycheckapp/services/notification_handler.py
+++ currencycheckproject/currencycheckapp/services/notification_handler.py
@@ -40,7 +40,7 @@ def get_currency_value(user, currency_shortcut) -> dict:
 @shared_task
 def get_threshold(user, currency_shortcut) -> dict:
     try:
-        user_currency = UserCurrencies.objects.filter(user=user, currency_shortcut=currency_shortcut).latest('id')
+        user_currency = UserCurrencies.objects.filter(user=user, currency_shortcut=currency_shortcut).order_by('-id').first()
         threshold = {'upper_limit': user_currency.upper_limit, 'lower_limit': user_currency.lower_limit}
         print(f'DEBUG: in func(get_threshold) for {user_currency} upper_limit: {threshold["upper_limit"]} lower_limit: {threshold["lower_limit"]}')
         return threshold
